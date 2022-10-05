e2ba22aa1ca3b3fd0186c3903c2f3c2169216e94
Sabrina Baccam <sabrinabaccam@gmail.com>
2022-10-05 00:19:36 -0500

    double solutions

--- tshirtSorter.js
+++ tshirtSorter.js
@@ -7,22 +7,38 @@ The above example would be returned sssssssmmmmmlllll.
 The given string will never include characters outside of the lowercase s, m, and l. The string will also never contain any spaces.
 */
 
+// function tshirtSorter(shirt) {
+//     const sm = []; // represents small and medium shirts
+//     const large = []; // represents large shirts
+//     for(let i = 0; i < shirt.length; i++) {
+//         if(shirt[i] === 's') {
+//             sm.unshift(shirt[i]);
+//         } else if (shirt[i] === 'm') {
+//             sm.push(shirt[i]);
+//         } else {
+//             large.push(shirt[i]);
+//         }
+//     }
+//     const sortedSM = sm.join('');
+//     const sortedL = large.join('');
+//     return sortedSM + sortedL;
+// }
+
+
 function tshirtSorter(shirt) {
-    const sm = []; // represents small and medium shirts
-    const large = []; // represents large shirts
-    for(let i = 0; i < shirt.length; i++) {
-        if(shirt[i] === 's') {
-            sm.unshift(shirt[i]);
-        } else if (shirt[i] === 'm') {
-            sm.push(shirt[i]);
-        } else {
-            large.push(shirt[i]);
+        let sm = ''; // represents small and medium shirts
+        let large = ''; // represents large shirts
+        for(let i = 0; i < shirt.length; i++) {
+            if(shirt[i] === 's') {
+                sm = shirt[i] + sm;
+            } else if (shirt[i] === 'm') {
+                sm += shirt[i];
+            } else {
+                large += shirt[i];
+            }
         }
+        return sm + large;
     }
-    const sortedSM = sm.join('');
-    const sortedL = large.join('');
-    return sortedSM + sortedL;
-}
 
 console.log(tshirtSorter('lms')); // sml
 console.log(tshirtSorter('smllms')); //ssmmll
